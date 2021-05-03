ccf9e11a54309800e6961aaa1d14158f6363f1a8
ottobonilla95 <mr.bonilla51@gmail.com>
2021-05-03 15:33:55 +0100

    webpackchanges

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   entry: path.resolve(__dirname, "/src/index.tsx"),
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, `dist${publicPath}`),
     filename: "bundle.js",
     publicPath: publicPath,
   },
